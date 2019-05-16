2d388c5ebbc2fa14a367232084eb0544ddb1aaac
Vineet Singh <44739429+bunny87@users.noreply.github.com>
2019-05-15 21:35:10 -0400

    Updated code file

--- TestGithubApi.py
+++ TestGithubApi.py
@@ -1,31 +1,38 @@
-"""
-@author: vineet singh
-@date: February 19, 2019
-The primary goal of this file is to demonstrate a simple unittest implementation
-"""
-
-import unittest
-from githubApi import getRepoCommits, res
-
-# This code implements the unit test functionality
-
-class TestGitHub(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
-
-    def testgetRepoCommits_Blank(self):
-        self.assertIsNone(getRepoCommits(''))
-
-    def testgetRepoCommits_InvalidValue(self):
-        self.assertIsNotNone(getRepoCommits('ABC'))
-
-    def testgetRepoCommits_ValidValue(self):
-        self.assertEqual(getRepoCommits('bunny87'), ['Repo: CS546, Number of commits: 2',
-                                                    'Repo: GithubApi567, Number of commits: 19',
-                                                    'Repo: SSW567, Number of commits: 4',
-                                                    'Repo: SSW810, Number of commits: 2',
-                                                    'Repo: Triangle567, Number of commits: 16'])
+import requests
+import json
 
 
-if __name__ == '__main__':
-    print('Running unit tests')
-    unittest.main()
+def Repocheck(UserID):
+    URL = 'https://api.github.com/users/'+UserID+'/repos'
+    html = requests.get(URL)
+    loaded_json = json.loads(html.text)
+    repos = []
+    for x in loaded_json:
+        if "name" in x:
+            repos.append([x["name"]])
+    for i in repos:
+        URL = "https://api.github.com/repos/"+UserID+"/"+i[0]+"/commits"
+        html = requests.get(URL)
+        loaded_json = json.loads(html.text)
+        commit = 0
+        for x in loaded_json:
+            if "commit" in x:
+                commit = commit + 1
+        i.append(commit)
+    return repos
+
+
+"""def Repoprint(repos):
+    for i in repos:
+        print(f"Repo: {i[0]} Number of commits: {i[1]}")
+    print(repos)"""
+
+
+#def main():
+#    UserID = input("Enter GitHub user ID: ")
+#    repos = Repocheck(UserID)
+#    Repoprint(repos)
+
+# Uncomment this below line to use this py file
+#if __name__ == "__main__":
+#    main()
