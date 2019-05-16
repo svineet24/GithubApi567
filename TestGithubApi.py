9747a5518e3a2c852dfd6c56deb0e56580205d7a
Vineet Singh <44739429+bunny87@users.noreply.github.com>
2019-05-15 21:41:52 -0400

    updated code file

--- TestGithubApi.py
+++ TestGithubApi.py
@@ -1,38 +1,51 @@
-import requests
-import json
+import unittest
+
+from HW4A_AdityaMunot import *
+from unittest import mock
 
 
-def Repocheck(UserID):
-    URL = 'https://api.github.com/users/'+UserID+'/repos'
-    html = requests.get(URL)
-    loaded_json = json.loads(html.text)
-    repos = []
-    for x in loaded_json:
-        if "name" in x:
-            repos.append([x["name"]])
-    for i in repos:
-        URL = "https://api.github.com/repos/"+UserID+"/"+i[0]+"/commits"
-        html = requests.get(URL)
-        loaded_json = json.loads(html.text)
-        commit = 0
-        for x in loaded_json:
-            if "commit" in x:
-                commit = commit + 1
-        i.append(commit)
-    return repos
+def call_this_one(url):
+
+    if url == 'https://api.github.com/users/AdityaMunot/repos':
+        return call('repos1.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/Anagram-Variety/commits":
+        return call('commitsAnagram-Variety.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/hello-world/commits":
+        return call('commitshello-world.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/RepoFetch/commits":
+        return call('commitsRepoFetch.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/Repositoryprogram/commits":
+        return call('commitsRepositoryprogram.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/SSW-555-Group-Project/commits":
+        return call('commitsSSW-555-Group-Project.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/SSW567/commits":
+        return call('commitsSSW567.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/Triangle567/commits":
+        return call('commitsTriangle567.json')
+    elif url == "https://api.github.com/repos/AdityaMunot/AWS-MERN/commits":
+        return call('commitsAWS-MERN.json')
 
 
-"""def Repoprint(repos):
-    for i in repos:
-        print(f"Repo: {i[0]} Number of commits: {i[1]}")
-    print(repos)"""
+def call(path):
+    data = Myclass()
+    with open(path, 'r') as f:
+        data.text = json.load(f)
+    return data
 
 
-#def main():
-#    UserID = input("Enter GitHub user ID: ")
-#    repos = Repocheck(UserID)
-#    Repoprint(repos)
+class Myclass:
+    text = ""
 
-# Uncomment this below line to use this py file
-#if __name__ == "__main__":
-#    main()
+
+class testrepos(unittest.TestCase):
+
+    @mock.patch('requests.get')
+    def testrepos(self, mocked_request):
+        mocked_request.side_effect = call_this_one
+
+        repos = Repocheck('AdityaMunot')
+        expect = [['Anagram-Variety', 14], ['AWS-MERN', 6], ['hello-world', 3], ['RepoFetch', 16], ['Repositoryprogram', 9], ['SSW-555-Group-Project', 30], ['SSW567', 1], ['Triangle567', 6]]
+        self.assertEqual(repos, expect)
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
